75680e7e602e530ef11cf02454a6067efa815d02
Anderson Tonin Thums <atthums@ucs.br>
2024-05-28 17:40:00 -0300

    criados ex13 e ex14

---
+++ aula13/ambiente.js
@@ -0,0 +1,17 @@
+var vez = 8
+while(vez) {
+    console.log(`Tudo bem?`)
+    vez--
+}
+
+var c = 1
+while (c <= 6){
+    console.log(`Passo ${c}`)
+    c++
+}
+
+var d = 1
+do {
+    console.log(`carro ${d}`)
+    d++
+} while (d <=6)
