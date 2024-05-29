7d76edf1b0a3890ed9c6de50791f130b00631034
Anderson Tonin Thums <atthums@ucs.br>
2024-05-28 21:04:53 -0300

    Realização exercicio 17

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,39 @@
-alert('Olá')
+function tabuada(){
+    var num = document.getElementById('txtn')
+    var tab = document.getElementById('seltab')
+    var res = document.getElementById('res')
+
+    if (num.value == 0){
+        alert(`Verifique os dados informados!!`)
+    } else{        
+        var n = Number(num.value) 
+        var c = 1
+        tab.innerHTML = ` `
+        while (c <= 10) {
+            var item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
+    }    
+}
+
+/*
+function tabuada(){
+    var num = document.getElementById('txtn')
+    var res = document.getElementById('res')
+    var mult
+    res.innerHTML = ` `
+
+    if (num.value == 0){
+        alert(`Verifique os dados informados!!`)
+    } else{        
+        var n = Number(num.value) 
+        for (c = 1; c < 11; c++){
+            mult = c * n
+            res.innerHTML += `${c} * ${n} = ${mult} <br>`
+        }
+    }
+    
+}*/
